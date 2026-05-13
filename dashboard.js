94ffec5d17c6d3b28d28098f278e81bcaed59ebf
franccescodelarosa-bit <franccescodelarosa@gmail.com>
2026-05-12 20:42:01 -0500

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,7 @@
+const token = localStorage.getItem("idToken");
+const userSub = localStorage.getItem("userSub");
 
+if (!token || !userSub) {
+    alert("Acceso denegado.");
+    window.location.href = "index.html";
+}
